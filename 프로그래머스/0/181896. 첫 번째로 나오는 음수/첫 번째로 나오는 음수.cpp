b509bf64842a911c2782aa907b70fdd5706b62ae
MinGu Jeong <51395707+MinGu-Jeong@users.noreply.github.com>
2024-05-24 18:20:23 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list) {
+    int answer = 0;
+    int size = num_list.size();
+    for(int i=0; i< size; i++){
+        if(num_list[i]<0)
+            return i;
+    }
+    return -1;
+}
