1c085fba9dbb594b73df7ec098b5c7f3e6b5ee90
MinGu Jeong <51395707+MinGu-Jeong@users.noreply.github.com>
2024-05-24 18:29:27 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+string solution(string my_string, int n) {
+    string answer = "";
+    return my_string.substr(0,n);
+}
