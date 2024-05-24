fc4c145d92438cd4f9fa3b329a1d4a496afaa859
MinGu Jeong <51395707+MinGu-Jeong@users.noreply.github.com>
2024-05-24 19:02:31 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 110.58 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+    int size = arr.size();
+    answer.push_back(arr[0]);
+    for(int i =1; i< size; i++){
+        if(arr[i]!=arr[i-1])
+            answer.push_back(arr[i]);
+    }
+    
+
+    return answer;
+}
