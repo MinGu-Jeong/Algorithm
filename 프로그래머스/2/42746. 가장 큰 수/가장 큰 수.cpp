32692c82628d06de2a31a9f8e913a5aa92669abc
MinGu Jeong <51395707+MinGu-Jeong@users.noreply.github.com>
2024-05-26 15:54:26 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+bool compare (string a, string b){
+    return a+b > b+a;
+}
+
+string solution(vector<int> numbers) {
+    string answer = "";
+    vector<string> str_numbers;
+    for(int num : numbers){
+        str_numbers.push_back(to_string(num));
+    }
+    sort(str_numbers.begin(),str_numbers.end(),compare);
+    for(string str : str_numbers){
+        answer += str;
+    }
+    if(answer[0]== '0'){
+        return "0";
+    }
+    
+    return answer;
+}
